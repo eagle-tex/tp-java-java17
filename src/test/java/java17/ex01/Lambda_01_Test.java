0a931d6ebe67cf8ac26819bd6e6912ba7b66299f
Gildas Montcho <gmontcho@gmail.com>
2024-11-22 16:10:36 +0100

    branche '02-lambda' - fin des exercices

--- src/test/java/java17/ex01/Lambda_01_Test.java
+++ src/test/java/java17/ex01/Lambda_01_Test.java
@@ -1,5 +1,6 @@
 package java17.ex01;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
 
 import java17.data.Data;
@@ -8,79 +9,86 @@ import java17.data.Person;
 import java.util.ArrayList;
 import java.util.List;
 
-
-/**
- * Exercice 01 - Filter
- */
+/** Exercice 01 - Filter */
 public class Lambda_01_Test {
 
-    // tag::PersonPredicate[]
-    interface PersonPredicate {
-        boolean test(Person p);
+  // tag::PersonPredicate[]
+  interface PersonPredicate {
+    boolean test(Person p);
+  }
+
+  // end::PersonPredicate[]
+
+  // tag::filter[]
+  private List<Person> filter(List<Person> persons, PersonPredicate predicate) {
+    List<Person> filteredPersons = new ArrayList<Person>();
+    for (Person p : persons) {
+      if (predicate.test(p)) {
+        filteredPersons.add(p);
+      }
     }
-    // end::PersonPredicate[]
+    return filteredPersons;
+  }
 
-    // tag::filter[]
-    private List<Person> filter(List<Person> persons, PersonPredicate predicate) {
-        List<Person> filteredPersons = new ArrayList<Person>();
-    	for (Person p: persons){
-        	if (predicate.test(p)){
-        		filteredPersons.add(p);
-        	}
-        }
-        return filteredPersons;
+  // end::filter[]
+
+  // tag::test_filter_by_age[]
+  @Test
+  public void test_filter_by_age() throws Exception {
+
+    List<Person> personList = Data.buildPersonList(100);
+
+    // TODO result ne doit contenir que des personnes adultes (age >= 18)
+    List<Person> result = filter(personList, p -> p.getAge() >= 18);
+
+    assert result.size() == 83;
+
+    for (Person person : result) {
+      assert person.getAge() > 17;
     }
-    // end::filter[]
+  }
 
+  // end::test_filter_by_age[]
 
-    // tag::test_filter_by_age[]
-    @Test
-    public void test_filter_by_age() throws Exception {
+  // tag::test_filter_by_firstname[]
+  @Test
+  public void test_filter_by_firstname() throws Exception {
 
-        List<Person> personList = Data.buildPersonList(100);
+    List<Person> personList = Data.buildPersonList(100);
 
-        // TODO result ne doit contenir que des personnes adultes (age >= 18)
-        List<Person> result = filter(personList, null);
+    // TODO result ne doit contenir que des personnes dont le prénom est "first_10"
+    List<Person> result = filter(personList, p -> p.getFirstname().equals("first_10"));
 
-        assert result.size() == 83;
+    assert result.size() == 1;
+    assert result.get(0).getFirstname().equals("first_10");
+  }
 
-        for (Person person : result) {
-            assert person.getAge() > 17;
-        }
+  // end::test_filter_by_firstname[]
+
+  // tag::test_filter_by_password[]
+  @Test
+  public void test_filter_by_password() throws Exception {
+
+    List<Person> personList = Data.buildPersonList(100);
+
+    String passwordSha512Hex =
+        "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff";
+
+    // TODO result ne doit contenir que les personnes dont l'age est > 49 et dont le hash du mot de
+    // passe correspond à la valeur de la variable passwordSha512Hex
+    // TODO Pour obtenir le hash d'un mot, utiliser la méthode DigestUtils.sha512Hex(mot)
+    List<Person> result =
+        filter(
+            personList,
+            p -> {
+              return (p.getAge() > 49)
+                  && (DigestUtils.sha512Hex(p.getPassword()).equals(passwordSha512Hex));
+            });
+
+    assert result.size() == 6;
+    for (Person person : result) {
+      assert person.getPassword().equals("test");
     }
-    // end::test_filter_by_age[]
-
-    // tag::test_filter_by_firstname[]
-    @Test
-    public void test_filter_by_firstname() throws Exception {
-
-        List<Person> personList = Data.buildPersonList(100);
-
-        // TODO result ne doit contenir que des personnes dont le prénom est "first_10"
-        List<Person> result = filter(personList, null);
-
-        assert result.size() == 1;
-        assert result.get(0).getFirstname().equals("first_10");
-
-    }
-    // end::test_filter_by_firstname[]
-
-    // tag::test_filter_by_password[]
-    @Test
-    public void test_filter_by_password() throws Exception {
-
-        List<Person> personList = Data.buildPersonList(100);
-
-        String passwordSha512Hex = "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff";
-
-        // TODO result ne doit contenir que les personnes dont l'age est > 49 et dont le hash du mot de passe correspond à la valeur de la variable passwordSha512Hex
-        // TODO Pour obtenir le hash d'un mot, utiliser la méthode DigestUtils.sha512Hex(mot)
-        List<Person> result = filter(personList, null);
-
-        assert result.size() == 6;
-        for (Person person : result) {
-            assert person.getPassword().equals("test");
-        }
-    }
-    // end::test_filter_by_password[]
+  }
+  // end::test_filter_by_password[]
 }
--- src/test/java/java17/ex02/Lambda_02_Test.java
+++ src/test/java/java17/ex02/Lambda_02_Test.java
@@ -9,42 +9,40 @@ import java17.data.Person;
 import java.util.ArrayList;
 import java.util.List;
 
-
-/**
- * Exercice 02 - Map
- */
+/** Exercice 02 - Map */
 public class Lambda_02_Test {
 
-    // tag::PersonToAccountMapper[]
-    interface PersonToAccountMapper {
-        Account map(Person p);
+  // tag::PersonToAccountMapper[]
+  interface PersonToAccountMapper {
+    Account map(Person p);
+  }
+
+  // end::PersonToAccountMapper[]
+
+  // tag::map[]
+  private List<Account> map(List<Person> personList, PersonToAccountMapper mapper) {
+    List<Account> accounts = new ArrayList<>();
+    personList.forEach(p -> accounts.add(mapper.map(p)));
+    return accounts;
+  }
+
+  // end::map[]
+
+  // tag::test_map_person_to_account[]
+  @Test
+  public void test_map_person_to_account() throws Exception {
+
+    List<Person> personList = Data.buildPersonList(100);
+
+    // TODO transformer la liste de personnes en liste de comptes
+    // TODO tous les objets comptes ont un solde à 100 par défaut
+    List<Account> result = map(personList, p -> new Account(p, 100));
+
+    assert result.size() == personList.size();
+    for (Account account : result) {
+      assert account.getBalance().equals(100);
+      assert account.getOwner() != null;
     }
-    // end::PersonToAccountMapper[]
-
-    // tag::map[]
-    private List<Account> map(List<Person> personList, PersonToAccountMapper mapper) {
-        List<Account> accounts = new ArrayList<>();
-        personList.forEach(p -> accounts.add(mapper.map(p)));
-        return accounts;
-    }
-    // end::map[]
-
-
-    // tag::test_map_person_to_account[]
-    @Test
-    public void test_map_person_to_account() throws Exception {
-
-        List<Person> personList = Data.buildPersonList(100);
-
-        // TODO transformer la liste de personnes en liste de comptes
-        // TODO tous les objets comptes ont un solde à 100 par défaut
-        List<Account> result = map(personList, null);
-
-        assert result.size() == personList.size();
-        for (Account account : result) {
-            assert account.getBalance().equals(100);
-            assert account.getOwner() != null;
-        }
-    }
-    // end::test_map_person_to_account[]
+  }
+  // end::test_map_person_to_account[]
 }
