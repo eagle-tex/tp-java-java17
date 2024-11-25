05a9c4b46f6d3f6490f91dad9c1e6183943a022c
Gildas Montcho <gmontcho@gmail.com>
2024-11-25 11:09:06 +0100

    branche '04-optional' - fin des exercices

--- src/test/java/java17/data/Person.java
+++ src/test/java/java17/data/Person.java
@@ -2,50 +2,62 @@ package java17.data;
 
 public class Person {
 
-    private String firstname;
-    private String lastname;
-    private Integer age;
-    private String password;
+  private String firstname;
+  private String lastname;
+  private Integer age;
+  private String password;
 
-    public Person() {
-    }
+  public Person() {}
 
-    public Person(String firstname, String lastname, Integer age, String password) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.age = age;
-        this.password = password;
-    }
+  public Person(String firstname, String lastname, Integer age, String password) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.age = age;
+    this.password = password;
+  }
 
-    public String getFirstname() {
-        return firstname;
-    }
+  public String getFirstname() {
+    return firstname;
+  }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
 
-    public String getLastname() {
-        return lastname;
-    }
+  public String getLastname() {
+    return lastname;
+  }
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
 
-    public Integer getAge() {
-        return age;
-    }
+  public Integer getAge() {
+    return age;
+  }
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
+  public void setAge(Integer age) {
+    this.age = age;
+  }
 
-    public String getPassword() {
-        return password;
-    }
+  public String getPassword() {
+    return password;
+  }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public String toString() {
+    return "Person { firstname = '"
+        + firstname
+        + "', lastname = '"
+        + lastname
+        + "', age = "
+        + age
+        + ", password='"
+        + password
+        + "' }";
+  }
 }
